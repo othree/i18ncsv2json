b11c49bf88fb096f45fb3448eb8d22626d867b79
othree <othree@gmail.com>
2016-01-20 17:21:51 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,80 @@
+
+var program = require('commander');
+
+
+program
+  .version('0.0.1')
+  .usage('[options] <file ...>')
+  .option('-p, --path [value]', 'output path', 'out')
+  .option('-d, --delimeter [value]', 'delimeter between filename and lang', '.')
+  .parse(process.argv);
+
+var file = program.args[0];
+
+if (!file) {
+   console.error('no csv file is given!');
+   process.exit(1);
+}
+
+var title = file.split('.').slice(0, -1).join('.');
+
+var fs = require('fs');
+var parse = require('csv-parse');
+
+fs.readFile(file, function (err, content) {
+  if (err) {
+     console.error('file is not readable!');
+     process.exit(1);
+  }
+
+  parse(content, function (err, trans) {
+    var o_langs = trans.shift();
+    o_langs.shift();
+
+    var langs = o_langs.slice();
+    var lang = '';
+
+    var buffer = {};
+
+    while (lang = langs.shift()) {
+      lang = lang.toLowerCase();
+      buffer[lang] = {};
+    }
+
+    var entry = [];
+    var key = '';
+
+    var objectPath = require('object-path');
+
+    while (entry = trans.shift()) {
+      key = entry.shift();
+
+      langs = o_langs.slice();
+      while (v = entry.shift(), lang = langs.shift()) {
+        lang = lang.toLowerCase();
+        objectPath.set(buffer[lang], key, v);
+      }
+    }
+
+    var path = require('path')
+    var p = path.join(program.path);
+
+    require('mkdirp')(p);
+
+    var target_file = '';
+    var target = '';
+
+    langs = o_langs.slice();
+    while (lang = langs.shift()) {
+      lang = lang.toLowerCase();
+      target = [title, lang].join(program.delimeter);
+      target = [target, 'json'].join('.');
+
+      target_file = path.join(p, target);
+
+      console.log(target_file)
+      fs.writeFile(target_file, JSON.stringify(buffer[lang], null, 2));
+    }
+
+  });
+});
